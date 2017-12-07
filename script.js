4e647c203be53225bf9c5fab50bf937be73764be
Riyaz <Riyaz>
2017-12-07 07:57:06 +0530

    modified search only performed when keyword not empty

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-$.getJSON('./data/data.json', function (data) {
-	var result = '<ul>'
-	$.each(data, function (key, value) {
-		result += '<li>';
-		result += '<h2>' + value.name + '</h2>';
-		result += '<p>' + value.bio + '</p>';
-		result += '<img src="' + value.img + '" alt="' + value.name + ' image">';
-		result += '</li>';
-	})
-	result+= '</ul>'
-	$('#searchResults').html(result);
+$('#searchString').on('input propertychange paste', function() {
+    var searchValue = $.trim($(this).val());
+    if (searchValue != '') {
+        $.getJSON('./data/data.json', function(data) {
+            var result = '<ul>'
+            $.each(data, function(key, value) {
+                result += '<li>';
+                result += '<h2>' + value.name + '</h2>';
+                result += '<p>' + value.bio + '</p>';
+                result += '<img src="' + value.img + '" alt="' + value.name + ' image">';
+                result += '</li>';
+            })
+            result += '</ul>'
+            $('#searchResults').html(result);
+        });
+    } else {
+    	result = '';
+    	$('#searchResults').html(result);
+    }
 });

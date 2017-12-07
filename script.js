179d5332d3042e218ef5bb9dfc28f02aac7ec998
Riyaz <Riyaz>
2017-12-07 08:05:49 +0530

    added search fn when user typed

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 $('#searchString').on('input propertychange paste', function() {
     var searchValue = $.trim($(this).val());
+    var reg = new RegExp(searchValue, 'i');
     if (searchValue != '') {
         $.getJSON('./data/data.json', function(data) {
             var result = '<ul>'
             $.each(data, function(key, value) {
-                result += '<li>';
-                result += '<h2>' + value.name + '</h2>';
-                result += '<p>' + value.bio + '</p>';
-                result += '<img src="' + value.img + '" alt="' + value.name + ' image">';
-                result += '</li>';
-            })
+                if (value.name.match(reg)) {
+                    result += '<li>';
+                    result += '<h2>' + value.name + '</h2>';
+                    result += '<img src="' + value.img + '" alt="' + value.name + ' image">';
+                    result += '<p>' + value.bio + '</p>';
+                    result += '</li>';
+                }
+            });
             result += '</ul>'
             $('#searchResults').html(result);
+
         });
     } else {
-    	result = '';
-    	$('#searchResults').html(result);
+        result = '';
+        $('#searchResults').html(result);
     }
 });
